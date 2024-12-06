8a3e0c70ab872b80510c357feb4f0227cf1c4564
Timothy Means <tbmeans2005@gmail.com>
2024-12-05 22:23:37 -0500

    controller like framework func

--- homeController.js
+++ homeController.js
@@ -1,5 +1,8 @@
 const model = require('./tvrecModel.js');
 const view = require('./homeView.js');
 
-exports.controller = { model, view };
+exports.controller = function(input) {
+    return view.render(model.query(input));
+}
 
+exports.initCat = view.initCat;
--- serv.js
+++ serv.js
@@ -8,17 +8,16 @@ const port = 8080;
 /* Controllers */
 const home = require('./homeController.js');
 
-const initItem = home.controller.view.initCat;
+const initItem = home.initCat;
 
 const serv = http.createServer((req, res) => {
 	try {
 		const reqIsForHome = req.url === '/';
 		const qobj = reqIsForHome && initItem || qstr.parse(req.url);
 		const qkey = Object.keys(qobj)[0];
-		const list = home.controller.model.query(qobj[qkey]);
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'text/html');
-		res.end(home.controller.view.render(list));
+		res.end(home.controller(qobj[qkey]));
 	} catch (e) {
 		res.statusCode = 404;
 		res.setHeader('Content-Type', 'text/plain');
