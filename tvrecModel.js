c30dd69e6c0fe623b31e7326c15b3c3c00024dcd
Timothy Means <tbmeans2005@gmail.com>
2024-12-06 16:16:03 -0500

    corrected conditional

--- tvrecModel.js
+++ tvrecModel.js
@@ -3,7 +3,8 @@ const db = require('./tvrecs.js');
 exports.query = function(keyword) {
 	if (keyword == undefined) {
 		return [];
-	} else if (keyword.match(/^all/i)) {
+	}
+	if (keyword.match(/^all/i)) {
 		return db.list;
 	}
 	return db.list.filter(rec => rec.tags.includes(keyword));
