92681fa5afb03cac7395d37548b8e16b1bbe3cdd
Timothy Means <tbmeans2005@gmail.com>
2024-12-10 18:12:41 -0500

    call render w/ addl param

--- homeController.js
+++ homeController.js
@@ -2,7 +2,7 @@ const model = require('./tvrecModel.js');
 const view = require('./homeView.js');
 
 exports.controller = function(input) {
-    return view.render(model.query(input));
+    return view.render(input, model.query(input));
 }
 
 exports.initCat = view.initCat;
