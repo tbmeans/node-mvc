b1c146ece0b0ad55830e82a4fb174d02c426fb14
Timothy Means <tbmeans2005@gmail.com>
2025-05-19 18:20:57 -0400

    May 11: repl. concat w interpl.

--- videoView.js
+++ videoView.js
@@ -1,30 +1,27 @@
 exports.render = function(id, title, isCc) {
-	const doc = [
-		'<!DOCTYPE html> <html lang="en"> <head> ',
-		'<meta charset="utf-8" /> ',
-		'<meta name="viewport" content="width=device-width, ',
-		'initial-scale=1" /> ',
-        '<title>' + title + '</title> '
-	].join('');
-	
-	const sty = [
-		'<style> ',
-        'body{ background: black; text-align: center; } ',
-        'video{ padding-top: 10rem; } ',
-        '</style> </head> '
-	].join('');
-
-    const track = ('<track default kind="captions" src="/Videos/' + id +
-            '.vtt">');
-	
-	const bod = [
-        '<body>',
-        '<video controls autoplay name="media">',
-        '<source src="/Videos/' + id + '.mp4" type="video/mp4">',
-        isCc > 0 && track || '',
-        '</video>',
-        '</body>'
-    ].join('');
-	
-	return doc + sty + bod + '</html>';
+	return (
+`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>${title}</title>
+    <style>
+      body {
+        background: black;
+        text-align: center;
+      }
+      video {
+        padding-top: 10rem;
+      }
+    </style>
+  </head>
+  <body>
+    <video controls autoplay name="media">
+      <source src="/Videos/${id}.mp4" type="video/mp4">
+      ${isCc > 0 && `<track default kind="captions" src="/Videos/${id}.vtt">` || ''}
+    </video>
+  </body>
+</html>`
+  );
 };
